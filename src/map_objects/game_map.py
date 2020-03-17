ed023eeb004dbedf7d9adecc6ef2f6c359133a37
Bogdan Bannov <bog07@bk.ru>
2020-03-17 22:12:24 +0300

    fixed map generation

--- src/map_objects/game_map.py
+++ src/map_objects/game_map.py
@@ -1,7 +1,7 @@
 from random import randint
 
-from map_objects.tile import Tile
 from map_objects.rectangle import Rect
+from map_objects.tile import Tile
 
 class GameMap:
     def __init__(self, width, height):
@@ -9,12 +9,10 @@ class GameMap:
         self.height = height
         self.tiles = self.initialize_tiles()
 
-
     def initialize_tiles(self):
         tiles = [[Tile(True) for y in range(self.height)] for x in range(self.width)]
 
         return tiles
-
 
     def make_map(self, max_rooms, room_min_size, room_max_size, map_width, map_height, player):
         rooms = []
@@ -65,10 +63,9 @@ class GameMap:
                         self.create_v_tunnel(prev_y, new_y, prev_x)
                         self.create_h_tunnel(prev_x, new_x, new_y)
 
-                # finally, append the new room to the list
+                        # finally, append the new room to the list
                 rooms.append(new_room)
                 num_rooms += 1
-
 
     def create_room(self, room):
         # go through the tiles in the rectangle and make them passable
@@ -77,21 +74,18 @@ class GameMap:
                 self.tiles[x][y].blocked = False
                 self.tiles[x][y].block_sight = False
 
-
     def create_h_tunnel(self, x1, x2, y):
         for x in range(min(x1, x2), max(x1, x2) + 1):
             self.tiles[x][y].blocked = False
             self.tiles[x][y].block_sight = False
 
-
     def create_v_tunnel(self, y1, y2, x):
         for y in range(min(y1, y2), max(y1, y2) + 1):
             self.tiles[x][y].blocked = False
-            self.tiles[x][y].block_tiles = False
-
+            self.tiles[x][y].block_sight = False
 
     def is_blocked(self, x, y):
         if self.tiles[x][y].blocked:
             return True
-        
+
         return False

6e8c465dd5f7d07aaa8b13254d14ca230838bdc6
Bogdan Bannov <bog07@bk.ru>
2020-03-18 23:41:10 +0300

    fifth lesson, kicking enemies without damage, turn-based moving

--- src/map_objects/game_map.py
+++ src/map_objects/game_map.py
@@ -1,7 +1,9 @@
+import tcod as libtcod
 from random import randint
 
 from map_objects.rectangle import Rect
 from map_objects.tile import Tile
+from entity import Entity
 
 class GameMap:
     def __init__(self, width, height):
@@ -9,12 +11,15 @@ class GameMap:
         self.height = height
         self.tiles = self.initialize_tiles()
 
+
     def initialize_tiles(self):
         tiles = [[Tile(True) for y in range(self.height)] for x in range(self.width)]
 
         return tiles
 
-    def make_map(self, max_rooms, room_min_size, room_max_size, map_width, map_height, player):
+
+    def make_map(self, max_rooms, room_min_size, room_max_size,
+                 map_width, map_height, player, entities, max_monsters_per_room):
         rooms = []
         num_rooms = 0
 
@@ -63,9 +68,11 @@ class GameMap:
                         self.create_v_tunnel(prev_y, new_y, prev_x)
                         self.create_h_tunnel(prev_x, new_x, new_y)
 
-                        # finally, append the new room to the list
+                self.place_entities(new_room, entities, max_monsters_per_room)
+                # finally, append the new room to the list
                 rooms.append(new_room)
                 num_rooms += 1
+
 
     def create_room(self, room):
         # go through the tiles in the rectangle and make them passable
@@ -74,15 +81,36 @@ class GameMap:
                 self.tiles[x][y].blocked = False
                 self.tiles[x][y].block_sight = False
 
+
     def create_h_tunnel(self, x1, x2, y):
         for x in range(min(x1, x2), max(x1, x2) + 1):
             self.tiles[x][y].blocked = False
             self.tiles[x][y].block_sight = False
 
+
     def create_v_tunnel(self, y1, y2, x):
         for y in range(min(y1, y2), max(y1, y2) + 1):
             self.tiles[x][y].blocked = False
             self.tiles[x][y].block_sight = False
+
+
+    def place_entities(self, room, entities, max_monsters_per_room):
+        # Get a random number of monsters
+        number_of_monsters = randint(0, max_monsters_per_room)
+
+        for i in range(number_of_monsters):
+            # Choose a random location in the room
+            x = randint(room.x1 + 1, room.x2 - 1)
+            y = randint(room.y1 + 1, room.y2 - 1)
+
+            if not any([entity for entity in entities if entity.x == x and entity.y == y]):
+                if randint(0, 100) < 80:
+                    monster = Entity(x, y, 'o', libtcod.desaturated_green, 'Orc', True)
+                else:
+                    monster = Entity(x, y, 'T', libtcod.darker_green, 'Troll', True)
+                
+                entities.append(monster)
+
 
     def is_blocked(self, x, y):
         if self.tiles[x][y].blocked:

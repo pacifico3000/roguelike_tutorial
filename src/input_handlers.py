cc56042243f2c15ee27cf60f938c05127a05869c
Bogdan Bannov <bog07@bk.ru>
2020-04-13 02:20:58 +0300

    eighth chapter, inventory items usage and dropping

--- src/input_handlers.py
+++ src/input_handlers.py
@@ -1,6 +1,18 @@
 import tcod as libtcod
 
-def handle_keys(key):
+from game_states import GameStates
+
+def handle_keys(key, game_state):
+    if game_state == GameStates.PLAYERS_TURN:
+        return handle_player_turn_keys(key)
+    elif game_state == GameStates.PLAYER_DEAD:
+        return handle_player_dead_keys(key)
+    elif game_state in (GameStates.SHOW_INVENTORY, GameStates.DROP_INVENTORY):
+        return handle_inventory_keys(key)
+
+    return {}
+
+def handle_player_turn_keys(key):
     key_char = chr(key.c)
 
     # Movement keys
@@ -24,6 +36,8 @@ def handle_keys(key):
         return {'pickup': True}
     elif key_char == 'i':
         return {'show_inventory': True}
+    elif key_char == 'd':
+        return {'drop_inventory': True}
     
     if key.vk == libtcod.KEY_ENTER and key.lalt:
         # Alt+Enter: toggle full screen
@@ -33,4 +47,34 @@ def handle_keys(key):
         return {'exit': True}
 
     # No key was pressed
+    return {}
+
+def handle_player_dead_keys(key):
+    key_char = chr(key.c)
+
+    if key_char == 'i':
+        return {'show_inventory': True}
+
+    if key.vk == libtcod.KEY_ENTER and key.lalt:
+        # Alt + Enter: toggle full screen
+        return {'fullscreen': True}
+    if key.vk == libtcod.KEY_ESCAPE:
+        # Exit the menu
+        return {'exit': True}
+    
+    return
+
+def handle_inventory_keys(key):
+    index = key.c - ord('a')
+
+    if index >= 0:
+        return {'inventory_index': index}
+
+    if key.vk == libtcod.KEY_ENTER and key.lalt:
+        # Alt + Enter: toggle full screen
+        return {'fullscreen': True}
+    elif key.vk == libtcod.KEY_ESCAPE:
+        # Exit the menu
+        return {'exit': True}
+    
     return {}
--- src/map_objects/game_map.py
+++ src/map_objects/game_map.py
@@ -8,6 +8,7 @@ from components.fighter import Fighter
 from components.ai import BasicMonster
 from components.item import Item
 from render_functions import RenderOrder
+from item_functions import heal
 
 class GameMap:
     def __init__(self, width, height):
@@ -128,7 +129,7 @@ class GameMap:
             y = randint(room.y1, room.y2 - 1)
 
             if not any([entity for entity in entities if entity.x == x and entity.y == y]):
-                item_component = Item()
+                item_component = Item(use_function=heal, amount=4)
                 item = Entity(x, y, '!', libtcod.violet, 'Healing Potion',
                               render_order=RenderOrder.ITEM, item=item_component)
                 entities.append(item)
